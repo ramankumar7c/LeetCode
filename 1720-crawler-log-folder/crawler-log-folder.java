e6ca12e8788f2bd938081716836dfbc5996493a4
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2024-07-10 06:41:03 +0530

    Time: 2 ms (44.79%) | Memory: 41.8 MB (81.60%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> stack = new Stack<>();
+
+        for (String log : logs) {
+            if (log.equals("./"))
+                continue;
+            else if (log.equals("../")) {
+                if (!stack.isEmpty())
+                    stack.pop();
+            } else
+                stack.push(log);
+        }
+        return stack.size();
+    }
+}
