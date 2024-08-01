8aa52eaab6c2e479d4588ccf8958642e87c1af5f
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2024-08-01 19:37:26 +0530

    Time: 4 ms (11.42%) | Memory: 42.6 MB (45.62%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        return (int) Arrays.stream(details).filter(detail -> Integer.parseInt(detail.substring(11, 13)) > 60).count();
+    }
+}
