f385291a8250548feee6867102bace63d5080e93
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2024-08-21 09:17:17 +0530

    Time: 27 ms (29.39%) | Memory: 43.5 MB (22.04%) - LeetSync

---
+++ 664-strange-printer/strange-printer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int strangePrinter(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = dp[i + 1][j] + 1;
+                for (int k = i + 1; k <= j; k++)
+                    if (s.charAt(k) == s.charAt(i))
+                        dp[i][j] = Math.min(dp[i][j], dp[i][k - 1] + (k + 1 <= j ? dp[k + 1][j] : 0));
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
