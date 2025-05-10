813772c5df2cd165bc44729641790961217578e2
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-05-10 21:26:32 +0530

    Time: 3 ms (88.73%) | Memory: 61.1 MB (24.88%) - LeetSync

---
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long minSum(int[] nums1, int[] nums2) {
+        long sum1 = 0;
+        long zero1 = 0;
+        for (int num : nums1) {
+            sum1 += num;
+            if (num == 0)
+                zero1++;
+        }
+
+        long sum2 = 0;
+        long zero2 = 0;
+        for (int num : nums2) {
+            sum2 += num;
+            if (num == 0)
+                zero2++;
+        }
+
+        if (zero1 == 0 && sum1 < sum2 + zero2)
+            return -1;
+
+        if (zero2 == 0 && sum2 < sum1 + zero1)
+            return -1;
+
+        return Math.max(sum1 + zero1, sum2 + zero2);
+    }
+}
