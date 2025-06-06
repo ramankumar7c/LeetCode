d9528204fb860a0891f0d745c514d65604e2ab72
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-06-06 20:41:37 +0530

    Time: 72 ms (86.71%) | Memory: 55.5 MB (38.46%) - LeetSync

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String robotWithString(String s) {
+        StringBuilder result = new StringBuilder();
+        Deque<Character> stack = new ArrayDeque<>();
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
+
+        for (char c : s.toCharArray()) {
+            stack.push(c);
+            freq[c - 'a']--;
+
+            char minChar = findMinChar(freq);
+
+            while (!stack.isEmpty() && stack.peek() <= minChar)
+                result.append(stack.pop());
+        }
+        while (!stack.isEmpty())
+            result.append(stack.pop());
+
+        return result.toString();
+    }
+
+    private char findMinChar(int[] freq) {
+        for (int i = 0; i < 26; i++)
+            if (freq[i] > 0)
+                return (char) ('a' + i);
+
+        return 'x';
+    }
+}
