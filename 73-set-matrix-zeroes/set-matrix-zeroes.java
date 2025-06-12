21f451b62779bc59c035ce6a60f2c837f8436a8a
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-06-12 14:57:58 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (96.73%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -15,6 +15,7 @@ class Solution {
                 firstColZero = true;
                 break;
             }
+            
         for (int i = 1; i < m; i++)
             for (int j = 1; j < n; j++)
                 if (matrix[i][j] == 0) {
