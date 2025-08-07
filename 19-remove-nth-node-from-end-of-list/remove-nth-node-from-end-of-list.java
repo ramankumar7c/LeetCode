5ba8bc68d3e61eb239d7e002928dd0936f6ad2db
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-08-07 10:42:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (58.93%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,28 +10,50 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int count = 0;
-        ListNode temp = head;
-        while (temp != null) {
-            count++;
-            temp = temp.next;
-        }
-        if (count == n) {
-            ListNode newHead = head.next;
-            return newHead;
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++)
+            fast = fast.next;
+
+        if (fast == null)
+            return head.next;
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
         }
 
-        ListNode temp1 = head;
-        int res = count - n;
-        while (temp1 != null) {
-            res--;
-            if (res == 0)
-                break;
-            temp1 = temp1.next;
-        }
-        ListNode newNode = temp1.next;
-        temp1.next = temp1.next.next;
+        ListNode newNode = slow.next;
+        slow.next = slow.next.next;
 
         return head;
     }
 }
+
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         int count = 0;
+//         ListNode temp = head;
+//         while (temp != null) {
+//             count++;
+//             temp = temp.next;
+//         }
+//         if (count == n) {
+//             ListNode newHead = head.next;
+//             return newHead;
+//         }
+
+//         ListNode temp1 = head;
+//         int res = count - n;
+//         while (temp1 != null) {
+//             res--;
+//             if (res == 0)
+//                 break;
+//             temp1 = temp1.next;
+//         }
+//         ListNode newNode = temp1.next;
+//         temp1.next = temp1.next.next;
+
+//         return head;
+//     }
+// }
