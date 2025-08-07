82919c081c98444cf2cdacd78bb2291fd2b72bc6
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-08-07 11:31:00 +0530

    Time: 9 ms (96.08%) | Memory: 57.2 MB (22.16%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -12,18 +12,70 @@ class Solution {
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null)
             return head;
-        List<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-        while (temp != null) {
-            list.add(temp.val);
-            temp = temp.next;
+
+        ListNode middle = middleNode(head);
+        ListNode right = middle.next;
+        middle.next = null;
+        ListNode left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+        return mergeTwoLists(left, right);
+    }
+
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        Collections.sort(list);
-        temp = head;
-        for (int val : list) {
-            temp.val = val;
-            temp = temp.next;
+        return prev;
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
         }
-        return head;
+        if (list1 != null)
+            current.next = list1;
+        else
+            current.next = list2;
+
+        return dummy.next;
     }
 }
+
+// Brute Force Solution
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+//         if (head == null || head.next == null)
+//             return head;
+//         List<Integer> list = new ArrayList<>();
+//         ListNode temp = head;
+//         while (temp != null) {
+//             list.add(temp.val);
+//             temp = temp.next;
+//         }
+//         Collections.sort(list);
+//         temp = head;
+//         for (int val : list) {
+//             temp.val = val;
+//             temp = temp.next;
+//         }
+//         return head;
+//     }
+// }
