d1f7d54397e1e79e844dd17ab567229b593d03ac
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-08-11 12:15:44 +0530

    Time: 1 ms (99.95%) | Memory: 43.5 MB (61.55%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -2,18 +2,39 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        ans.add(new ArrayList<>());
-
-        for (int num : nums) {
-            int size = ans.size();
-            for (int i = 0; i < size; i++) {
-                List<Integer> newSubset = new ArrayList<>(ans.get(i));
-                newSubset.add(num);
-                ans.add(newSubset);
-            }
-        }
-        Set<List<Integer>> set = new HashSet<>(ans);
-        ans = new ArrayList<>(set);
+        findSubsets(0, nums, new ArrayList<>(), ans);
         return ans;
     }
+
+    private void findSubsets(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(ds));
+        for (int i = index; i < nums.length; i++) {
+            if (i != index && nums[i] == nums[i - 1])
+                continue;
+            ds.add(nums[i]);
+            findSubsets(i + 1, nums, ds, ans);
+            ds.remove(ds.size() - 1);
+        }
+    }
 }
+
+// Brute Force Solution
+// class Solution {
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+//         Arrays.sort(nums);
+//         List<List<Integer>> ans = new ArrayList<>();
+//         ans.add(new ArrayList<>());
+
+//         for (int num : nums) {
+//             int size = ans.size();
+//             for (int i = 0; i < size; i++) {
+//                 List<Integer> newSubset = new ArrayList<>(ans.get(i));
+//                 newSubset.add(num);
+//                 ans.add(newSubset);
+//             }
+//         }
+//         Set<List<Integer>> set = new HashSet<>(ans);
+//         ans = new ArrayList<>(set);
+//         return ans;
+//     }
+// }
