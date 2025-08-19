3667af3679b1dc4e4ad06ea084b625573f7d68bc
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-08-19 21:10:33 +0530

    Time: 1 ms (69.43%) | Memory: 41.1 MB (35.91%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -6,12 +6,8 @@ class Solution {
         if (dividend == Integer.MIN_VALUE && divisor == -1)
             return Integer.MAX_VALUE;
 
-        boolean sign = true;
+        boolean sign = (dividend >= 0) == (divisor >= 0);
 
-        if (dividend >= 0 && divisor < 0)
-            sign = false;
-        else if (dividend <= 0 && divisor > 0)
-            sign = false;
         long n = Math.abs((long) dividend);
         long d = Math.abs((long) divisor);
         long quotient = 0;
