ccbdf254e6c6b6a6e8b330d0dc89e08837cd2ba3
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-09-27 18:36:30 +0530

    Time: 7 ms (68.15%) | Memory: 42 MB (11.77%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.java
+++ 830-largest-triangle-area/largest-triangle-area.java
@@ -5,9 +5,9 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             int[] A = points[i];
-            for (int j = 0; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {
                 int[] B = points[j];
-                for (int k = 0; k < n; k++) {
+                for (int k = j + 1; k < n; k++) {
                     int[] C = points[k];
                     double area = 0.5 * Math.abs((B[0] - A[0]) * (C[1] - A[1]) - (C[0] - A[0]) * (B[1] - A[1]));
                     maxArea = Math.max(maxArea, area);
