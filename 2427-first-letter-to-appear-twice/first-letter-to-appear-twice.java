afa3048c0bc0be1fcb057aeb3a84e9bffc58829f
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-09-28 08:49:42 +0530

    Time: 1 ms (13.40%) | Memory: 41.7 MB (47.05%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,15 +1,27 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        char[] ch = s.toCharArray();
-        int index = Integer.MAX_VALUE;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j < s.length(); j++) {
-                if (ch[i] == ch[j]) {
-                    index = Math.min(index, j);
-                    break;
-                }
-            }
+        Set<Character> seen = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            if (seen.contains(c))
+                return c;
+            seen.add(c);
         }
-        return ch[index];
+        return ' ';
     }
 }
+
+// class Solution {
+//     public char repeatedCharacter(String s) {
+//         char[] ch = s.toCharArray();
+//         int index = Integer.MAX_VALUE;
+//         for (int i = 0; i < s.length(); i++) {
+//             for (int j = i + 1; j < s.length(); j++) {
+//                 if (ch[i] == ch[j]) {
+//                     index = Math.min(index, j);
+//                     break;
+//                 }
+//             }
+//         }
+//         return ch[index];
+//     }
+// }
