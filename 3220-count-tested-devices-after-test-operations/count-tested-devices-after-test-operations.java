006d2038bc5ee4f7ba2f616308dd61500556c112
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-09-29 15:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (39.90%) - LeetSync

--- 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.java
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.java
@@ -3,17 +3,10 @@ class Solution {
         int n = batteryPercentages.length;
         int count = 0;
         for (int i = 0; i < n; i++) {
-            if (batteryPercentages[i] > 0) {
+            if (batteryPercentages[i] - count > 0) {
                 count++;
-                decrease(batteryPercentages, i + 1);
             }
         }
         return count;
-    }
-
-    private void decrease(int[] arr, int index) {
-        for (int i = index; i < arr.length; i++) {
-            arr[i] = Math.max(0, arr[i] - 1);
-        }
     }
 }
