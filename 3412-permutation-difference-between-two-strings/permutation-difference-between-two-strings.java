888f0618027a1fba394eb06cc9df116aed556cb0
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-09-29 15:20:02 +0530

    Time: 2 ms (52.56%) | Memory: 43.4 MB (11.42%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.java
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.java
@@ -3,14 +3,12 @@ class Solution {
         int sum = 0;
         Map<Character, Integer> map = new HashMap<>();
 
-        char[] sArray = s.toCharArray();
         for (int i = 0; i < s.length(); i++) {
-            map.put(sArray[i], i);
+            map.put(s.charAt(i), i);
         }
 
-        char[] tArray = t.toCharArray();
         for (int i = 0; i < t.length(); i++) {
-            sum += Math.abs(i - map.get(tArray[i]));
+            sum += Math.abs(i - map.get(t.charAt(i)));
         }
 
         return sum;
