17fa8a85626dc9c1b6093913aeaa22ba542f620f
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-10-27 23:49:47 +0530

    Time: 1 ms (15.98%) | Memory: 41.1 MB (22.87%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingWeight(int n) {
+        String binary = Integer.toBinaryString(n);
+        int count = 0;
+        for (char c : binary.toCharArray())
+            if (c == '1')
+                count++;
+
+        return count;
+    }
+}
