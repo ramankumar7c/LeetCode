fd02591346a6f9551ea8aa394c4639b0d1c6c322
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-10-29 10:29:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (10.44%) - LeetSync

---
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String toHex(int num) {
+        if (num == 0)
+            return "0";
+        String[] strs = new String[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            sb.append(strs[num & 15]);
+            num >>>= 4;
+        }
+        return sb.reverse().toString();
+    }
+}
