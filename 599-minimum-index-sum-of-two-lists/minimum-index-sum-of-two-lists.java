907ffd1ad72998fe575d0977348c64d02b388e9a
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-11-01 23:01:36 +0530

    Time: 9 ms (49.61%) | Memory: 47.5 MB (9.68%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.java
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.java
@@ -4,27 +4,21 @@ class Solution {
         for (int i = 0; i < list1.length; i++) {
             map1.put(list1[i], i);
         }
-        ArrayList<Integer> commonIndices = new ArrayList<>();
-        for (int i = 0; i < list2.length; i++) {
-            if (map1.containsKey(list2[i])) {
-                commonIndices.add(i);
-            }
-        }
+
         int leastSum = Integer.MAX_VALUE;
         List<String> ansList = new ArrayList<>();
-        for (int indices : commonIndices) {
-            int currSum = indices + map1.get(list2[indices]);
-            if (currSum < leastSum) {
-                leastSum = currSum;
-                ansList.clear();
-                ansList.add(list2[indices]);
-            } else if (currSum == leastSum)
-                ansList.add(list2[indices]);
+        for (int i = 0; i < list2.length; i++) {
+            if (map1.containsKey(list2[i])) {
+                int currSum = i + map1.get(list2[i]);
+                if (currSum < leastSum) {
+                    leastSum = currSum;
+                    ansList.clear();
+                    ansList.add(list2[i]);
+                } else if (currSum == leastSum)
+                    ansList.add(list2[i]);
+            }
         }
-        String[] ans = new String[ansList.size()];
-        for (int i = 0; i < ansList.size(); i++) {
-            ans[i] = ansList.get(i);
-        }
-        return ans;
+
+        return ansList.toArray(new String[0]);
     }
 }
