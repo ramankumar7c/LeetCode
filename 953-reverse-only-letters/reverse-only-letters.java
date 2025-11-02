3a58fe226033b55367a49915183769d0dc0e5424
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-11-02 12:10:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (5.28%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -1,20 +1,42 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
-        StringBuilder letters = new StringBuilder();
-        for (char c : s.toCharArray())
-            if (Character.isLetter(c))
-                letters.append(c);
-        letters.reverse();
-        String letter = letters.toString();
-        StringBuilder ans = new StringBuilder();
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        int l = 0, r = n - 1;
 
-        int n = s.length(), j = 0;
-        for (int i = 0; i < n; i++) {
-            if (Character.isLetter(s.charAt(i)))
-                ans.append(letter.charAt(j++));
-            else
-                ans.append(s.charAt(i));
+        while (l < r) {
+            if (!Character.isLetter(arr[l]))
+                l++;
+            else if (!Character.isLetter(arr[r]))
+                r--;
+            else {
+                char temp = arr[l];
+                arr[l++] = arr[r];
+                arr[r--] = temp;
+            }
         }
-        return ans.toString();
+        return new String(arr);
     }
 }
+
+// First Thought Solution
+// class Solution {
+//     public String reverseOnlyLetters(String s) {
+//         StringBuilder letters = new StringBuilder();
+//         for (char c : s.toCharArray())
+//             if (Character.isLetter(c))
+//                 letters.append(c);
+//         letters.reverse();
+//         String letter = letters.toString();
+//         StringBuilder ans = new StringBuilder();
+
+//         int n = s.length(), j = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (Character.isLetter(s.charAt(i)))
+//                 ans.append(letter.charAt(j++));
+//             else
+//                 ans.append(s.charAt(i));
+//         }
+//         return ans.toString();
+//     }
+// }
