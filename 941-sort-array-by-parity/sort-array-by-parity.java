b08541bb50de6c464491b1981d1e6f377dae3d61
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-11-02 12:19:25 +0530

    Time: 1 ms (49.56%) | Memory: 46.8 MB (5.16%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -3,12 +3,12 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         int l = 0, r = n - 1;
-        int i = 0;
-        while (i < n) {
+
+        for (int i = 0; i < n; i++) {
             if (nums[i] % 2 == 0)
-                ans[l++] = nums[i++];
+                ans[l++] = nums[i];
             else
-                ans[r--] = nums[i++];
+                ans[r--] = nums[i];
         }
         return ans;
     }
