2aea1809c7423b4f0eee5fe37f90d9ac8d0768ad
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-11-05 19:21:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (5.71%) - LeetSync

--- 3625-stone-removal-game/stone-removal-game.java
+++ 3625-stone-removal-game/stone-removal-game.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean canAliceWin(int n) {
+        if (n >= 55)
+            return false;
         for (int stones = 10; stones >= 0; stones--) {
             if (stones > n)
                 return stones % 2 == 1;
