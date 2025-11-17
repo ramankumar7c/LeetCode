16b8b1b9a7ea0384352bdd737b33645c04dc9b16
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-11-17 10:24:17 +0530

    Time: 1 ms (99.77%) | Memory: 47.5 MB (27.36%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1];
+        int duplicate = -1;
+        for (int num : nums) {
+            if (seen[num]) {
+                duplicate = num;
+            }
+            seen[num] = true;
+        }
+
+        int missing = -1;
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                missing = i;
+                break;
+            }
+        }
+        return new int[]{duplicate, missing};
+    }
+}
