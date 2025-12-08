b78f598b1737f00ef7095473f3e95e8e5746a8c6
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-12-08 20:50:51 +0530

    Time: 2 ms (81.06%) | Memory: 47.4 MB (44.77%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if (arr.length < 3)
+            return false;
+
+        int l = 0, r = arr.length - 1;
+        while (l < r && arr[l + 1] > arr[l])
+            l++;
+        while (r > 0 && arr[r] < arr[r - 1])
+            r--;
+
+        return l > 0 && r < arr.length - 1 && l == r;
+    }
+}
