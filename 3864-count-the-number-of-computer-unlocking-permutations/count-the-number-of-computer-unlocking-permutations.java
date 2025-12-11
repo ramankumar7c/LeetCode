4d54665d88fbbdfa1a1df6f901b6f87f0f858b3a
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-12-11 12:41:46 +0530

    Time: 2 ms (75.32%) | Memory: 92.1 MB (12.99%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
@@ -1,57 +1,16 @@
 class Solution {
-    static final long MOD = 1_000_000_007;
+    private static final int MOD = 1_000_000_007;
 
-    public int countPermutations(int[] comp) {
-        int n = comp.length;
-
-        int[] minParent = new int[n];
-        Arrays.fill(minParent, -1);
-
-        int minComp = comp[0];
-        int minIdx = 0;
-
-        for (int i = 1; i < n; i++) {
-
-            if (minComp < comp[i])
-                minParent[i] = minIdx;
-            else
-                return 0;
-
-            if (comp[i] < minComp) {
-                minComp = comp[i];
-                minIdx = i;
-            }
-        }
-
-        List<List<Integer>> bucket = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            bucket.add(new ArrayList<>());
-
+    public int countPermutations(int[] complexity) {
+        int n = complexity.length;
         for (int i = 1; i < n; i++)
-            bucket.get(minParent[i]).add(i);
-
-        long ways = 1;
-        Queue<Integer> available = new ArrayDeque<>();
-
-        for (int x : bucket.get(0))
-            available.add(x);
-
-        int unlocked = 1;
-
-        while (unlocked < n) {
-            int A = available.size();
-            if (A == 0)
+            if (complexity[i] <= complexity[0])
                 return 0;
 
-            ways = (ways * A) % MOD;
+        long ans = 1;
+        for (int i = 1; i < n; i++)
+            ans = (ans * i) % MOD;
 
-            int pick = available.poll();
-            unlocked++;
-
-            for (int x : bucket.get(pick))
-                available.add(x);
-        }
-
-        return (int) ways;
+        return (int) ans;
     }
 }
