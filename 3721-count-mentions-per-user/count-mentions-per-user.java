a18dd8143fd9c477d4d1f0585917bea85b026535
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-12-12 21:42:33 +0530

    Time: 34 ms (43.20%) | Memory: 53.4 MB (5.60%) - LeetSync

---
+++ 3721-count-mentions-per-user/count-mentions-per-user.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[] countMentions(int numberOfUsers, List<List<String>> events) {
+        record OfflineUser(int returnTimestamp, int userId) {
+        }
+        int[] ans = new int[numberOfUsers];
+        boolean[] online = new boolean[numberOfUsers];
+        Arrays.fill(online, true);
+
+        Queue<OfflineUser> offlineQueue = new PriorityQueue<>(Comparator.comparingInt(OfflineUser::returnTimestamp));
+        int allMentionsCount = 0;
+
+        events.sort(
+                Comparator.comparingInt((List<String> event) -> Integer.parseInt(event.get(1)))
+                        .thenComparing((List<String> event) -> event.get(0), Comparator.reverseOrder()));
+
+        for (List<String> event : events) {
+            final String eventType = event.get(0);
+            final int timestamp = Integer.parseInt(event.get(1));
+
+            while (!offlineQueue.isEmpty() && offlineQueue.peek().returnTimestamp <= timestamp)
+                online[offlineQueue.poll().userId] = true;
+
+            if (eventType.equals("MESSAGE")) {
+                String mentionsString = event.get(2);
+                if (mentionsString.equals("ALL")) {
+                    ++allMentionsCount;
+                } else if (mentionsString.equals("HERE")) {
+                    for (int userId = 0; userId < numberOfUsers; ++userId)
+                        if (online[userId])
+                            ++ans[userId];
+                } else {
+                    for (final int userId : getUserIds(mentionsString))
+                        ++ans[userId];
+                }
+            } else if (eventType.equals("OFFLINE")) {
+                final int userId = Integer.parseInt(event.get(2));
+                online[userId] = false;
+                offlineQueue.offer(new OfflineUser(timestamp + 60, userId));
+            }
+        }
+
+        for (int userId = 0; userId < numberOfUsers; ++userId)
+            ans[userId] += allMentionsCount;
+
+        return ans;
+    }
+
+    private List<Integer> getUserIds(final String s) {
+        List<Integer> integers = new ArrayList<>();
+        for (String part : s.split(" "))
+            integers.add(Integer.parseInt(part.substring(2)));
+        return integers;
+    }
+}
