116c42fb521fdcfcf87f770ab86245577efd9b94
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2025-12-29 13:57:35 +0530

    Time: 1 ms (41.68%) | Memory: 43 MB (11.30%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] results = new String[words.length];
+
+        for (String word : words) {
+            int pos = word.charAt(word.length() - 1) - '1';
+            results[pos] = word.substring(0, word.length() - 1);
+        }
+
+        return String.join(" ", results);
+    }
+}
