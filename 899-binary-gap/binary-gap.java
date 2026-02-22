c5b3c74121e40b788917a839978b97522fc32959
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2026-02-23 00:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (30.35%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int binaryGap(int n) {
+        int lastPosition = -1;
+        int maxDistance = 0;
+        int position = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (lastPosition != -1) {
+                    maxDistance = Math.max(maxDistance, position - lastPosition);
+                }
+                lastPosition = position;
+            }
+
+            position++;
+            n >>= 1;
+        }
+
+        return maxDistance;
+    }
+}
