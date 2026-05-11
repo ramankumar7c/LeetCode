196d9a12179faa3424ed4b97c822f4230df9b6fa
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2026-05-11 21:49:20 +0530

    Time: 5 ms (77.69%) | Memory: 46.7 MB (36.46%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -1,15 +1,30 @@
 class Solution {
     public int[] separateDigits(int[] nums) {
         List<Integer> list = new ArrayList<>();
-        String str = Arrays.toString(nums);
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) >= '0' && str.charAt(i) <= '9')
-                list.add(Character.getNumericValue(str.charAt(i)));
-        }
+        for (int num : nums)
+            for (char c : String.valueOf(num).toCharArray())
+                list.add(c - '0');
+
         int[] result = new int[list.size()];
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < result.length; i++)
             result[i] = list.get(i);
-        }
+
         return result;
     }
 }
+
+// class Solution {
+//     public int[] separateDigits(int[] nums) {
+//         List<Integer> list = new ArrayList<>();
+//         String str = Arrays.toString(nums);
+//         for (int i = 0; i < str.length(); i++) {
+//             if (str.charAt(i) >= '0' && str.charAt(i) <= '9')
+//                 list.add(Character.getNumericValue(str.charAt(i)));
+//         }
+//         int[] result = new int[list.size()];
+//         for (int i = 0; i < result.length; i++) {
+//             result[i] = list.get(i);
+//         }
+//         return result;
+//     }
+// }
