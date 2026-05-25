5aa395e1bc0cf47c0c54f5a63c1aba95906c6a23
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2026-05-25 18:16:28 +0530

    Time: 8 ms (86.86%) | Memory: 47.9 MB (59.33%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        boolean[] dp = new boolean[n];
+
+        dp[0] = true;
+
+        int pre = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (i >= minJump && dp[i - minJump])
+                pre++;
+
+            if (i > maxJump && dp[i - maxJump - 1])
+                pre--;
+
+            if (pre > 0 && s.charAt(i) == '0')
+                dp[i] = true;
+        }
+        return dp[n - 1];
+    }
+}
