4df30b746cdb893648723fdca78d728e71be845a
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2026-05-31 14:34:04 +0530

    Time: 25 ms (93.30%) | Memory: 107.3 MB (65.13%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long m = mass;
+        for (int asteroid : asteroids) {
+            if (m >= asteroid)
+                m += asteroid;
+            else
+                return false;
+        }
+        return true;
+    }
+}
