86946bd3ac2cd8d83388bac92755976b31e50270
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2026-06-28 11:26:53 +0530

    Time: 8 ms (91.25%) | Memory: 49 MB (73.29%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,18 +1,23 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1)
+            return intervals;
+            
+        List<int[]> result = new ArrayList<>();
+
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int[] current = intervals[0];
 
-        Stack<int[]> stack = new Stack<>();
-
-        for (int[] interval : intervals) {
-            if (stack.isEmpty() || stack.peek()[1] < interval[0])
-                stack.push(interval);
-            else {
-                int[] top = stack.pop();
-                top[1] = Math.max(top[1], interval[1]);
-                stack.push(top);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] next = intervals[i];
+            if (next[0] <= current[1]) {
+                current[1] = Math.max(current[1], next[1]);
+            } else {
+                result.add(current);
+                current = next;
             }
         }
-        return stack.toArray(new int[stack.size()][]);
+        result.add(current);
+        return result.toArray(new int[result.size()][]);
     }
 }
