1b6101c4c7dde2b2266509366dc69e008c0e68bc
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2026-07-15 10:57:06 +0530

    Time: 1 ms (60.60%) | Memory: 47.5 MB (91.49%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -16,3 +16,30 @@ class Solution {
         return ans;
     }
 }
+
+// TC = O(3N) SC = O(2N)
+// class Solution {
+//     public int trap(int[] height) {
+//         int n = height.length;
+
+//         int[] prefixMax = new int[n];
+//         int[] suffixMax = new int[n];
+
+//         prefixMax[0] = height[0];
+//         for (int i = 1; i < n; i++)
+//             prefixMax[i] = Math.max(prefixMax[i - 1], height[i]);
+
+//         suffixMax[n - 1] = height[n - 1];
+//         for (int i = n - 2; i >= 0; i--)
+//             suffixMax[i] = Math.max(suffixMax[i + 1], height[i]);
+
+//         int total = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             if (height[i] < prefixMax[i] && height[i] < suffixMax[i])
+//                 total += Math.min(prefixMax[i], suffixMax[i]) - height[i];
+//         }
+
+//         return total;
+//     }
+// }
