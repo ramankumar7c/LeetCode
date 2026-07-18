a2a47594b4db0224513d77968225e60835b86df3
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2026-07-18 16:31:21 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (90.91%) - LeetSync

---
+++ 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String findLatestTime(String s) {
+        char[] t = s.toCharArray();
+
+        if (t[0] == '?' && t[1] == '?') {
+            t[0] = '1';
+            t[1] = '1';
+        } else if (t[0] == '?') {
+            if (t[1] <= '1')
+                t[0] = '1';
+            else
+                t[0] = '0';
+        } else if (t[1] == '?') {
+            if (t[0] == '1')
+                t[1] = '1';
+            else
+                t[1] = '9';
+        }
+
+        if (t[3] == '?')
+            t[3] = '5';
+        if (t[4] == '?')
+            t[4] = '9';
+
+        return new String(t);
+    }
+
+}
