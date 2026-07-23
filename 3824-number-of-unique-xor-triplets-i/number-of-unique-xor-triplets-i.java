96aee39cb94d7a8c5d85d38f704dfa11231fb441
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2026-07-23 22:35:35 +0530

    Time: 1 ms (51.02%) | Memory: 125.4 MB (53.06%) - LeetSync

---
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if (n < 3)
+            return n;
+        int x = (int) (Math.log(n) / Math.log(2));
+        return 1 << (x + 1);
+    }
+}
