27d1803f59cd33c1d7e7bad5d82ba33398485be6
Raman Kumar <114894525+ramankumar7c@users.noreply.github.com>
2026-09-21 21:08:41 +0530

    Time: 1 ms (26.95%) | Memory: 43.2 MB (20.20%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,5 +1,22 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder result=new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+
+            if(c>='A' && c<= 'Z')
+            c=(char) (c+32);
+
+            result.append(c);
+        }
+
+        return result.toString();
     }
 }
+
+// class Solution {
+//     public String toLowerCase(String s) {
+//         return s.toLowerCase();
+//     }
+// }
